96a4c00feaca9170f1ac73393e4e965fb208d256
Noy <noyloy@gmail.com>
2018-06-05 20:28:21 +0300

    job duration
    claculate duration of jobs for pipe

--- src/app/pipes/job.pipe.ts
+++ src/app/pipes/job.pipe.ts
@@ -19,8 +19,22 @@ export class JobPipe implements PipeTransform {
 
   getJobTimeFormat(job: Job) {
     const datePipe: DatePipe = new DatePipe('en-US');
-    let endTime =  job.end_time == null ? 'Now' : datePipe.transform(job.end_time, 'MMM yyyy');
-    return datePipe.transform(job.start_time, 'MMM yyyy')+ ' - ' + endTime;
+    let endTimeStr =  job.end_time == null ? 'Present' : datePipe.transform(job.end_time, 'MMM yyyy');
+    let endTime = job.end_time == null ? (new Date()) : job.end_time;
+    let durationStr = this.getJobTimeDurationStr(job.start_time,endTime);
+    return datePipe.transform(job.start_time, 'MMM yyyy')+ ' - ' + endTimeStr + '(' + durationStr +')';
+  }
+
+  getJobTimeDurationStr(start_time : Date, end_time : Date) {
+    let jobDuration = end_time.getTime() - start_time.getTime();
+    let dayInMillisec = 1000*60*60*24;
+    let days = jobDuration/dayInMillisec;
+    let months = Math.ceil(days/31) % 12;
+    let years = Math.floor(((days/31)-months) /12);
+    if (months == 0 && years == 0) return "1 mos";
+    if (years == 0) return months+ " mos";
+    if (months == 0) return years + " yr";
+    return years + " yr " + months + " mos";
   }
 
 }
